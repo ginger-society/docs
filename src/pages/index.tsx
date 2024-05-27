55a65ef159829cc879c2b99b8785878db7f75374
Ginger Society Admin <admin@gingersociety.org>
2024-05-27 16:48:13 +0530

    chore: cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/category/ginger-dj"
           >
             See how our docs are structured and how to use them.
           </Link>
